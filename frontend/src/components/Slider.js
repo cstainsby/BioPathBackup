70aace1a55371d2d8fa9cb4c7402e43501d5c430
bhigley <bhigley@zagmail.gonzaga.edu>
2022-10-05 12:10:34 -0700

    sliders with working values added

--- frontend/src/components/Slider.js
+++ frontend/src/components/Slider.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react'
 import './Slider.css'
 
 export default class Slider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 5};
+  }
+
+  handleChange(e) {
+    this.setState((state, props) => ({
+      value: e
+    }));
+  }
+
   render() {
     return (
       <div className='Slider'>
         {this.props.title}
+        <input type="range"
+          min={0}
+          max={10}
+          onChange={(e) => this.handleChange(e.target.value)}
+          value={this.state.value}
+          />
+          <h1>{this.state.value}</h1>
       </div>
     )
   }

cbe7bc4cbd5bd6e8bf232ea21f468c6a0fff4085
Zach Burnaby <58493767+TheRedLancer@users.noreply.github.com>
2022-10-13 16:31:07 -0700

    added first part of glycolysis

--- frontend/src/components/ModelArea.js
+++ frontend/src/components/ModelArea.js
@@ -11,6 +11,10 @@ import ReactFlow, {
 import 'reactflow/dist/style.css'
 
 const groupHeight = 130;
+const groupWidth1 = 120;
+const groupWidth2 = 230;
+const substrateHeight = 50
+const substrateWidth = 100
 
 const initialNodes = [
   { 
@@ -19,7 +23,7 @@ const initialNodes = [
     data: {label: 'Hexokinase'},
     type: 'group',
     style: {
-      width: 170,
+      width: groupWidth2,
       height: groupHeight,
     },
   },
@@ -29,7 +33,7 @@ const initialNodes = [
     data: {label: ''},
     type: 'group',
     style: {
-      width: 170,
+      width: groupWidth1,
       height: groupHeight,
     },
   },
@@ -39,7 +43,7 @@ const initialNodes = [
     data: {label: null},
     type: 'group',
     style: {
-      width: 170,
+      width: groupWidth2,
       height: groupHeight,
     },
   },
@@ -49,17 +53,17 @@ const initialNodes = [
     data: {label: null},
     type: 'group',
     style: {
-      width: 170,
+      width: groupWidth2,
       height: groupHeight,
     },
   },
   { 
     id: '5e',
-    position: { x: 0, y: (groupHeight + 20) * 4 },
+    position: { x: substrateWidth + 10, y: (groupHeight + 20) * 4 },
     data: {label: null},
     type: 'group',
     style: {
-      width: 170,
+      width: groupWidth1,
       height: groupHeight,
     },
   },
@@ -69,218 +73,321 @@ const initialNodes = [
     data: {label: null},
     type: 'group',
     style: {
-      width: 170,
+      width: groupWidth2,
       height: groupHeight,
     },
   },
   { 
-    id: '7e',
-    position: { x: 0, y: (groupHeight + 20) * 6 },
-    data: {label: null},
-    type: 'group',
+    id: 'starts',
+    position: { x: 10, y: -70 },
+    data: { label: 'Glucose' },
     style: {
-      width: 170,
-      height: groupHeight,
+      width: substrateWidth,
+      height: substrateHeight,
     },
   },
-  { 
-    id: '8e',
-    position: { x: 0, y: (groupHeight + 20) * 7 },
-    data: {label: null},
-    type: 'group',
-    style: {
-      width: 170,
-      height: groupHeight,
-    },
-  },
-  { 
-    id: '9e',
-    position: { x: 0, y: (groupHeight + 20) * 8 },
-    data: {label: null},
-    type: 'group',
-    style: {
-      width: 170,
-      height: groupHeight,
-    },
-  },
-  { 
-    id: '10e',
-    position: { x: 0, y: (groupHeight + 20) * 9},
-    data: {label: null},
-    type: 'group',
-    style: {
-      width: 170,
-      height: groupHeight,
-    },
-  },
-  { 
+  {
     id: '1s',
     position: { x: 10, y: 10 },
     data: { label: 'Glucose' },
     parentNode: '1e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   { 
     id: '2s',
-    position: { x: 10, y: 60 },
-    data: { label: 'Glucose-6-phosphate' },
+    position: { x: 10, y: substrateHeight + 20 },
+    data: { label: 'G6P' },
     parentNode: '1e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
+  },
+  {
+    id: '1sc',
+    position: { x: substrateWidth + 20, y: 10 },
+    data: { label: 'ATP' },
+    parentNode: '1e',
+    extent: 'parent',
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
+  },
+  { 
+    id: '2sc',
+    position: { x: substrateWidth + 20, y: substrateHeight + 20 },
+    data: { label: 'ADP' },
+    parentNode: '1e',
+    extent: 'parent',
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   { 
     id: '3s',
     position: { x: 10, y: 10 },
-    data: { label: 'Glucose-6-phosphate' },
+    data: { label: 'G6P' },
     parentNode: '2e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   {
     id: '4s',
-    position: { x: 10, y: 60 },
-    data: { label: 'Fructose-6-phosphate' },
+    position: { x: 10, y: substrateHeight + 20 },
+    data: { label: 'F6P' },
     parentNode: '2e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   {
     id: '5s',
     position: { x: 10, y: 10 },
-    data: { label: 'Fructose-6-phosphate' },
+    data: { label: 'F6P' },
     parentNode: '3e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   {
     id: '6s',
-    position: { x: 10, y: 60 },
-    data: { label: 'Fructose-1,6-bisphosphate' },
+    position: { x: 10, y: substrateHeight + 20 },
+    data: { label: 'F1,6BP' },
     parentNode: '3e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
+  },
+  {
+    id: '3sc',
+    position: { x: substrateWidth + 20, y: 10 },
+    data: { label: 'ATP' },
+    parentNode: '3e',
+    extent: 'parent',
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
+  },
+  { 
+    id: '4sc',
+    position: { x: substrateWidth + 20, y: substrateHeight + 20 },
+    data: { label: 'ADP' },
+    parentNode: '3e',
+    extent: 'parent',
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   { 
     id: '7s',
     position: { x: 10, y: 10 },
-    data: { label: 'Glucose' },
+    data: { label: 'F1,6BP' },
     parentNode: '4e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   { 
     id: '8s',
-    position: { x: 10, y: 60 },
-    data: { label: 'Glucose-6-phosphate' },
+    position: { x: 10, y: substrateHeight + 20 },
+    data: { label: 'GH3P'},
     parentNode: '4e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
+  },
+  { 
+    id: '8s2',
+    position: { x: substrateWidth + 20, y: substrateHeight + 20 },
+    data: { label: 'DHAP' },
+    parentNode: '4e',
+    extent: 'parent',
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   { 
     id: '9s',
     position: { x: 10, y: 10 },
-    data: { label: 'Glucose' },
+    data: { label: 'DHAP' },
     parentNode: '5e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   { 
     id: '10s',
-    position: { x: 10, y: 60 },
-    data: { label: 'Glucose-6-phosphate' },
+    position: { x: 10, y: substrateHeight + 20 },
+    data: { label: 'GH3P' },
     parentNode: '5e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   { 
     id: '11s',
     position: { x: 10, y: 10 },
-    data: { label: 'Glucose' },
+    data: { label: 'GH3P' },
     parentNode: '6e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   { 
     id: '12s',
-    position: { x: 10, y: 60 },
-    data: { label: 'Glucose-6-phosphate' },
+    position: { x: 10, y: substrateHeight + 20 },
+    data: { label: '1,3BPG' },
     parentNode: '6e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
+  },
+  {
+    id: '5sc',
+    position: { x: substrateWidth + 20, y: 10 },
+    data: { label: 'NAD' },
+    parentNode: '6e',
+    extent: 'parent',
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   { 
-    id: '13s',
-    position: { x: 10, y: 10 },
-    data: { label: 'Glucose' },
-    parentNode: '7e',
+    id: '6sc',
+    position: { x: substrateWidth + 20, y: substrateHeight + 20 },
+    data: { label: 'NADH' },
+    parentNode: '6e',
     extent: 'parent',
-    draggable: false,
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
   { 
-    id: '14s',
-    position: { x: 10, y: 60 },
-    data: { label: 'Glucose-6-phosphate' },
-    parentNode: '7e',
-    extent: 'parent',
-    draggable: false,
-  },
-  { 
-    id: '15s',
-    position: { x: 10, y: 10 },
-    data: { label: 'Glucose' },
-    parentNode: '8e',
-    extent: 'parent',
-    draggable: false,
-  },
-  { 
-    id: '16s',
-    position: { x: 10, y: 60 },
-    data: { label: 'Glucose-6-phosphate' },
-    parentNode: '8e',
-    extent: 'parent',
-    draggable: false,
-  },
-  { 
-    id: '17s',
-    position: { x: 10, y: 10 },
-    data: { label: 'Glucose' },
-    parentNode: '9e',
-    extent: 'parent',
-    draggable: false,
-  },
-  { 
-    id: '18s',
-    position: { x: 10, y: 60 },
-    data: { label: 'Glucose-6-phosphate' },
-    parentNode: '9e',
-    extent: 'parent',
-    draggable: false,
-  },
-  { 
-    id: '19s',
-    position: { x: 10, y: 10 },
-    data: { label: 'Glucose' },
-    parentNode: '10e',
-    extent: 'parent',
-    draggable: false,
-  },
-  { 
-    id: '20s',
-    position: { x: 10, y: 60 },
-    data: { label: 'Glucose-6-phosphate' },
-    parentNode: '10e',
-    extent: 'parent',
-    draggable: false,
+    id: 'ends',
+    position: { x: 10, y: (groupHeight + 20) * 6 },
+    data: { label: '1,3BPG' },
+    style: {
+      width: substrateWidth,
+      height: substrateHeight,
+    },
   },
 ];
 
 const initialEdges = [
   {
-    id: 'e1-2',
+    id: 'starts-1s',
+    source: 'starts',
+    target: '1s',
+    animated: true,
+    style: {
+      strokeWidth: 10,
+      stroke: 'red'
+    }
+  },
+  {
+    id: '2s-3s',
     source: '2s',
-    target: '3s' 
-  }
+    target: '3s',
+    animated: true,
+    style: {
+      strokeWidth: 10,
+      stroke: 'red'
+    }
+  },
+  {
+    id: '4s-5s',
+    source: '4s',
+    target: '5s',
+    animated: true,
+    style: {
+      strokeWidth: 10,
+      stroke: 'red'
+    }
+  },
+  {
+    id: '6s-7s',
+    source: '6s',
+    target: '7s',
+    animated: true,
+    style: {
+      strokeWidth: 10,
+      stroke: 'red'
+    }
+  },
+  {
+    id: '8s2-9s',
+    source: '8s2',
+    target: '9s',
+    animated: true,
+    style: {
+      strokeWidth: 10,
+      stroke: 'red'
+    }
+  },
+  {
+    id: '10s-11s',
+    source: '10s',
+    target: '11s',
+    animated: true,
+    style: {
+      strokeWidth: 10,
+      stroke: 'red'
+    }
+  },
+  {
+    id: '8s-11s',
+    source: '8s',
+    target: '11s',
+    animated: true,
+    style: {
+      strokeWidth: 10,
+      stroke: 'red'
+    }
+  },
+  {
+    id: '12s-ends',
+    source: '12s',
+    target: 'ends',
+    animated: true,
+    style: {
+      strokeWidth: 10,
+      stroke: 'red'
+    }
+  },
+  
 ];
 
 export default function ModelArea() {

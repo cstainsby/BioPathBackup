5df80912b3365c3469308a9a3dc76ff4bf3e2a41
cstainsby <cstainsby@zagmail.gonzaga.edu>
2022-10-19 11:00:40 -0700

    changes made to the local_deploy file

--- AWS/local-deploy/local_deploy.py
+++ AWS/local-deploy/local_deploy.py
@@ -1,8 +1,19 @@
+"""This script file will be used for deploying to AWS while our github actions are down
+    
+  To run this file: python3 local_deploy.py <options>
+  
+  Options include flags for what you want to be run:
+    --images-to-ecr 
+        will build, tag and push the projects images to ECR
+    --deploy-infra 
+        will deploy the AWS infrastructure stacks to AWS
+    
+  NOTE: The default run with no flags will run everything, if you include one flag
+        only the specified job will be run"""
+
+
 import os
 import sys
-
-# simple python script to automate deployment while github actions isn't completely 
-# set up
 
 # repository information 
 # get updated info by command ""
@@ -39,15 +50,10 @@ BACKEND_PATH = os.path.join("../", "../", "backend/")
 FRONTEND_PATH = os.path.join("../", "../", "frontend/")
 INFRA_PATH = os.path.join("../", "infrastructure/")
 
-
-print(BACKEND_PATH)
-print(FRONTEND_PATH)
-print(INFRA_PATH)
-
 # credentials for deployment
 # import credentials file from credentials_<name>.txt
 
-# AWS_ACCT_ID = 0
+AWS_ACCT_ID = 0
 
 def console_job(job_title="", command="", desc=""):
   print("-----------------------------------------------------")
@@ -59,69 +65,105 @@ def console_job(job_title="", command="", desc=""):
   if desc != "":
     print(desc)
 
-  print("-----------------------done--------------------------")
+  print("-----------------------done--------------------------", end="\n\n")
 
 
-# build the frontend and backend images
-console_job(
-  "Building Frontend Docker Image", 
-  f"docker build -t {FRONTEND_IMG_NAME}:{FRONTEND_TAG} ../../frontend/"
-)
-console_job(
-  "Building Backend Docker Image", 
-  f"docker build -t {BACKEND_IMG_NAME}:{BACKEND_TAG} ../../backend/"
-)
-
-# tag the images 
-console_job(
-  "Tagging Frontend Image",
-  f"docker tag {FRONTEND_IMG_NAME}:{FRONTEND_TAG} {ECR_REPO_URI}:{FRONTEND_TAG}"
-)
-console_job( 
-  "Tagging Backend Image",
-  f"docker tag {BACKEND_IMG_NAME}:{BACKEND_TAG} {ECR_REPO_URI}:{BACKEND_TAG}"
-)
-
-# login to AWS
-# print("-----------------------------------------------------")
-# print("     Logging Into AWS Using Credentials.txt")
-# print("-----------------------------------------------------")
-# os.system(f"""aws ecr get-login-password --region {REGION_NAME} \
-#               | docker login \
-#                 --username AWS \
-#                 --password-stdin {AWS_ACCT_ID}.dkr.ecr.{REGION_NAME}.amazonaws.com""")
-
-# push the images to ECR
-console_job(
-  "Push Frontend Image to ECR",
-  f"docker push {ECR_REPO_URI}:{FRONTEND_TAG}"
-)
-console_job(
-  "Push Backend Image to ECR",
-  f"docker push {ECR_REPO_URI}:{BACKEND_TAG}"
-)
 
 
-# deploy VPC stack to AWS
-console_job(
-  "Deploy VPC Stack to Console",
-  f"aws cloudformation create-stack --stack-name vpc --template-body file://{INFRA_PATH}VPC.yaml"
-)
+def login_to_aws():
+  # login to AWS
+  console_job("Logging Into AWS",
+              f"""aws ecr get-login-password --region {REGION_NAME} \
+                | docker login \
+                  --username AWS \
+                  --password-stdin {AWS_ACCT_ID}.dkr.ecr.{REGION_NAME}.amazonaws.com""",
+              "Using information fielded from Credentials.txt, Logging you into AWS console")
 
-# deploy the credentials (IAM-Roles) stack
-console_job(
-  "Deploy IAM Roles Stack",
-  f"aws cloudformation create-stack --stack-name iam --template-body file://{INFRA_PATH}IAM-Roles.yaml --capabilities CAPABILITY_IAM"
-)
 
-# deploy the cluster
-console_job(
-  "Deploy Cluster Stack",
-  f"aws cloudformation create-stack --stack-name cluster --template-body file://{INFRA_PATH}Cluster.yaml"
-)
+def images_to_ecr():
+  # build the frontend and backend images
+  console_job(
+    "Building Frontend Docker Image", 
+    f"docker build -t {FRONTEND_IMG_NAME}:{FRONTEND_TAG} ../../frontend/"
+  )
+  console_job(
+    "Building Backend Docker Image", 
+    f"docker build -t {BACKEND_IMG_NAME}:{BACKEND_TAG} ../../backend/"
+  )
 
-# deploy the container stack
-console_job(
-  "Deploy Container Stack",
-  f"aws cloudformation create-stack --stack-name containers --template-body file://{INFRA_PATH}Containers.yaml"
-)
+  # tag the images 
+  console_job(
+    "Tagging Frontend Image",
+    f"docker tag {FRONTEND_IMG_NAME}:{FRONTEND_TAG} {ECR_REPO_URI}:{FRONTEND_TAG}"
+  )
+  console_job( 
+    "Tagging Backend Image",
+    f"docker tag {BACKEND_IMG_NAME}:{BACKEND_TAG} {ECR_REPO_URI}:{BACKEND_TAG}"
+  )
+
+  # push the images to ECR
+  console_job(
+    "Push Frontend Image to ECR",
+    f"docker push {ECR_REPO_URI}:{FRONTEND_TAG}"
+  )
+  console_job(
+    "Push Backend Image to ECR",
+    f"docker push {ECR_REPO_URI}:{BACKEND_TAG}"
+  )
+
+  #TODO: clean up locally built images
+  console_job(
+    "Clean up Locally Built Images",
+    f"docker rmi {FRONTEND_TAG} {BACKEND_TAG}"
+  )
+
+def deploy_aws_infrastructure():
+  # deploy VPC stack to AWS
+  console_job(
+    "Deploy VPC Stack to Console",
+    f"aws cloudformation create-stack --stack-name vpc --template-body file://{INFRA_PATH}VPC.yaml"
+  )
+
+  # deploy the credentials (IAM-Roles) stack
+  console_job(
+    "Deploy IAM Roles Stack",
+    f"aws cloudformation create-stack --stack-name iam --template-body file://{INFRA_PATH}IAM-Roles.yaml --capabilities CAPABILITY_IAM"
+  )
+
+  # deploy the cluster
+  console_job(
+    "Deploy Cluster Stack",
+    f"aws cloudformation create-stack --stack-name cluster --template-body file://{INFRA_PATH}Cluster.yaml"
+  )
+
+  # deploy the container stack
+  console_job(
+    "Deploy Container Stack",
+    f"aws cloudformation create-stack --stack-name containers --template-body file://{INFRA_PATH}Containers.yaml"
+  )
+
+
+def main():
+  flags = []
+
+  if len(sys.argv) - 1 > 0:
+    # there are flags that need to be handled
+    flags = sys.argv[1:]
+
+  #TODO: might need to change where this is placed depending on job, for now everything requires that you be signed in
+  login_to_aws()
+  
+
+  for flag in flags:
+    if flag == "--images-to-ecr":
+      images_to_ecr()
+    elif flag == "--deploy-infra":
+      deploy_aws_infrastructure()
+    else:
+      print("Error: unregcognized flag {}".format(flag))
+      return -1
+
+
+
+if __name__ == "__main__":
+  main()

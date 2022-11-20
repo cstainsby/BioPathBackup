c2608e8e2d952861c66e746c1b0c78db540e6027
Zach Burnaby <58493767+TheRedLancer@users.noreply.github.com>
2022-11-20 14:49:38 -0800

    Update urls.py

--- backend/biopath/urls.py
+++ backend/biopath/urls.py
@@ -8,29 +8,8 @@ Modified: 11/17 - Josh Schmitz
 from django.contrib import admin
 from django.urls import include, path
 
-<<<<<<< HEAD
-
 urlpatterns = [
     path(route='admin/', view=admin.site.urls),
     path(route='api/', view=include('api.urls')), # this essentially just includes the urls from api/urls.py
 
 ]
-
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('biopath/', include('biopath.urls')),
-#     path('frontend/', include('frontend.urls')),
-#     path('admin/', admin.site.urls),
-#     path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-#     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-# ]
-=======
-
-urlpatterns = [
-    path(route='admin/', view=admin.site.urls),
-    path(route='api/', view=include('api.urls')),
-]
->>>>>>> dev

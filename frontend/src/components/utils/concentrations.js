3d190ff638adcd237bf3a872808c9128f6719cfa
zach.burnaby <58493767+TheRedLancer@users.noreply.github.com>
2023-01-19 13:14:08 -0800

    add tests for concentration manager

--- frontend/src/components/utils/concentrations.js
+++ frontend/src/components/utils/concentrations.js
@@ -1,6 +1,21 @@
+/**
+ * @class
+ * @classdesc Managing model concentrations for
+ * real time web app.
+ * 
+ * @author zburnaby
+ * 
+ */
 export default class Concentrations {
+    /**
+     * @constructor
+     * @param {Object[]} enzymes list of enzymes
+     * @param enzymes[].substrates list of input moleules to the enzyme
+     * @param enzymes[].products list of output molecules to the enzyme
+     * @param enzymes[].cofactors list of molecules effecting the enzyme's production
+     */
     constructor(enzymes) {
-        /**
+        /*
          * {
          *  substrates: ["G", "ATP"],
          *  products: ["G6", "ADP"],
@@ -9,10 +24,18 @@ export default class Concentrations {
          */
         this.enzymes = enzymes;
         this.molecule_concentrations = [];
-        initConcentrations(this.enzymes);
+        this.initConcentrations(this.enzymes);
         this.listeners = [];
     }
 
+    /**
+     * Initializes the molecule_concentrations list given some enzymes
+     * @function
+     * @param {Object[]} enzymes list of enzymes
+     * @param enzymes[].substrates list of input moleules to the enzyme
+     * @param enzymes[].products list of output molecules to the enzyme
+     * @param enzymes[].cofactors list of molecules effecting the enzyme's production
+     */
     initConcentrations(enzymes) {
         for (const enzyme of enzymes) {
             for (const substrate of enzyme.substrates) {
@@ -27,6 +50,10 @@ export default class Concentrations {
         }
     }
 
+    /**
+     * Updates the concentrations of each molecule
+     * @function
+     */
     updateConcentrations() {
         let oldConcentrations = this.molecule_concentrations;
         for (const enzyme of this.enzymes) {
@@ -44,13 +71,34 @@ export default class Concentrations {
             }
         }
         for (const listener of this.listeners) {
-            listener.onConcentrationUpdate(this.molecule_concentrations);
+            listener(this.molecule_concentrations);
         }
     }
 
+    /**
+     * @callback onUpdateConcentration
+     * @param {Object[]} molecule_concentrations
+     */
+
+    /**
+     * Add listener function from listener queue
+     * @function
+     * @param {onUpdateConcentration} listener
+     */
     addListener(listener) {
-        if (Object.hasOwn(listener, 'onConcentrationUpdate')) {
-            this.listeners.push(listener);
+        this.listeners.push(listener);
+    }
+
+    /**
+     * Remove listener function from listener queue
+     * @function
+     * @param {onUpdateConcentration} listener
+     */
+    removeListener(listener) {
+        for (let i = 0; i < this.listeners.length; i++) {
+            if (this.listeners[i] === listener) {
+                this.listeners.splice(i, 1);
+            }
         }
     }
 }

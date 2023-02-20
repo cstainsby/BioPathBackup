95eb5fc45984abbcb76d7330e458d97d8950d942
cstainsby <cstainsby@zagmail.gonzaga.edu>
2023-02-19 20:42:42 -0800

    user changes before dev merge

--- backend/biopath/urls.py
+++ backend/biopath/urls.py
@@ -20,6 +20,5 @@ urlpatterns = [
     # register/ - used for sending json containing new user's data to be stored 
     # login/    - used for getting a sign in token from backend
     path(route='register/', view=UserRegistrationView.as_view(), name='register user'),
-    # path(route='login/', view=LoginView.as_view(), name='login'),
     path(route='api-token-auth/', view=views.obtain_auth_token)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

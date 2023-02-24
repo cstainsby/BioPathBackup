a04e9a6075bed7d560ac3b98378ba2205a577c24
cstainsby <cstainsby@zagmail.gonzaga.edu>
2023-02-23 16:44:49 -0800

    env vars changed

--- frontend/src/requestLib/requestConfig.js
+++ frontend/src/requestLib/requestConfig.js
@@ -1,6 +1,6 @@
 // ----------------------------------------------------------------------
 // requestConfig.js
-//  This file will contain the definitions of all endpoints that we will
+//  This file will contain the prefixes of all endpoints that we will
 //  be using in the project. There will also be a getter function to 
 //  grab the defined endpoint header. All of the request folder will use
 //  use the returned definition to direct their calls
@@ -10,18 +10,32 @@
 //     actual build
 // ----------------------------------------------------------------------
 
-const BACKEND_BASE_API_URL = "http://localhost:8000/"; 
-const MOCK_API_URL = "http://localhost:4000/";
-
-const userDefinedURL = BACKEND_BASE_API_URL;
-
 /**
  * getter for the user defined endpoint
  * @returns string representation of URL 
  */
 function getEndpointHeader() {
-  console.log("defined header: ", userDefinedURL);
-  return userDefinedURL;
+  const BACKEND_BASE_API_URL = "http://localhost:8000/"; 
+  const MOCK_API_URL = "http://localhost:4000/";
+  const AWS_BACKEND_URL = "";
+
+  let definedHeader = BACKEND_BASE_API_URL;
+
+  if (process.env.buildType === "production") {
+    // when container pushed to production 
+    definedHeader = AWS_BACKEND_URL;
+  }
+  else if (process.env.buildType === "local mock test") {
+    // endpoint for using the frontendJsonTestServer
+    definedHeader = MOCK_API_URL;
+  }
+  else if (process.env.buildType === "local backend") {
+    // this is the standard build type for when running the backend 
+    //    and local db with the frontend through docker-compose 
+    definedHeader = BACKEND_BASE_API_URL;
+  }
+  
+  return definedHeader;
 }
 
 export default getEndpointHeader;

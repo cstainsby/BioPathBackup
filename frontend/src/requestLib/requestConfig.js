d724ad2b1c2c7b35a9b3b08c577209685afac694
Josh Schmitz <joshschmitz8.8@gmail.com>
2023-04-18 20:30:34 -0700

    done

--- frontend/src/requestLib/requestConfig.js
+++ frontend/src/requestLib/requestConfig.js
@@ -43,7 +43,6 @@ function getEndpointHeader() {
   if(!definedBackendEndpoint || definedBackendEndpoint === "") {
     definedBackendEndpoint = localBackendEndpoint;
   }
-  definedBackendEndpoint = "http://localhost:80/"
   return definedBackendEndpoint;
 }
 

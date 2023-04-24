798c41718050badbb40f61f132fcbc8b8664ebfa
Zach Burnaby <58493767+TheRedLancer@users.noreply.github.com>
2023-04-23 21:35:41 -0700

    fix splash page error when no backend data

--- frontend/src/components/pages/SplashPage.js
+++ frontend/src/components/pages/SplashPage.js
@@ -8,107 +8,118 @@ import UserContext from "../../UserContext";
  * @returns A Splash page component
  */
 const SplashPage = () => {
-  const splashPageLoaderData = useLoaderData(); // get data loaded from react router
-  const { user, setUser } = useContext(UserContext);
+    const splashPageLoaderData = useLoaderData(); // get data loaded from react router
+    const { user, setUser } = useContext(UserContext);
 
-  const [recentWork, setRecentWork] = useState(null);
-  const [feed, setFeed] = useState(null);
+    const [recentWork, setRecentWork] = useState(null);
+    const [feed, setFeed] = useState(null);
 
-  useEffect(() => {
-    if (splashPageLoaderData.recentWork)
-      setRecentWork(splashPageLoaderData.recentWork);
+    useEffect(() => {
+        if (splashPageLoaderData.recentWork)
+            setRecentWork(splashPageLoaderData.recentWork);
 
-    if (splashPageLoaderData.feed) setFeed(splashPageLoaderData.feed);
-  }, [splashPageLoaderData]);
+        if (splashPageLoaderData.feed) setFeed(splashPageLoaderData.feed);
+    }, [splashPageLoaderData]);
 
-  return (
-    <div className="container">
-      <div className="fs-1">Biopath</div>
-      <div className="fs-5">A Biochemistry Interactive Learning Tool</div>
-
-      {/* Pick up where you left off section
-                Note: this should only display if the user is signed in */}
-      {user && (
+    return (
         <div className="container">
-          <div className="row justify-content-between border-bottom border-2 border-secondary py-2">
-            <div className="fs-3 col-auto">Jump Back In</div>
-            <Link
-              className="btn btn-primary col-auto me-2"
-              to={"user/" + user.username}
-            >
-              To Your Work
-            </Link>
-          </div>
+            <div className="fs-1">Biopath</div>
+            <div className="fs-5">A Biochemistry Interactive Learning Tool</div>
 
-          <div className="row pt-2 mx-sm-2">
-            {splashPageLoaderData.recentWork.slice(0, 4).map((pathway) => {
-              return (
-                <Link
-                  key={pathway.id}
-                  to={"pathway/" + pathway.id}
-                  className="col-auto card text-decoration-none mx-1 px-3 py-2"
-                >
-                  <div className="fs-4">{pathway.name}</div>
-                  <small className="text-muted">
-                    Created By {pathway.author}
-                  </small>
-                </Link>
-              );
-            })}
-          </div>
-        </div>
-      )}
+            {/* Pick up where you left off section
+                Note: this should only display if the user is signed in */}
+            {user && (
+                <div className="container">
+                    <div className="row justify-content-between border-bottom border-2 border-secondary py-2">
+                        <div className="fs-3 col-auto">Jump Back In</div>
+                        <Link
+                            className="btn btn-primary col-auto me-2"
+                            to={"user/" + user.username}
+                        >
+                            To Your Work
+                        </Link>
+                    </div>
 
-      <div className="container">
-        <div className="row border-bottom border-2 border-secondary py-2">
-          <div className="col-auto fs-3">Validated Pathways</div>
-        </div>
-        {splashPageLoaderData.recentWork.map((pathway) => {
-          return (
-            <Link
-              key={pathway.id}
-              to={"pathway/" + pathway.id}
-              className="row card text-decoration-none m-1 p-1 m-sm-2 p-sm-2"
-            >
-              <div className="fs-4 fw-bold">{pathway.name}</div>
-              <small className="text-muted">Created By {pathway.author}</small>
-            </Link>
-          );
-        })}
-      </div>
+                    {splashPageLoaderData.recentWork && (
+                        <div className="row pt-2 mx-sm-2">
+                            {splashPageLoaderData.recentWork
+                                .slice(0, 4)
+                                .map((pathway) => {
+                                    return (
+                                        <Link
+                                            key={pathway.id}
+                                            to={"pathway/" + pathway.id}
+                                            className="col-auto card text-decoration-none mx-1 px-3 py-2"
+                                        >
+                                            <div className="fs-4">
+                                                {pathway.name}
+                                            </div>
+                                            <small className="text-muted">
+                                                Created By {pathway.author}
+                                            </small>
+                                        </Link>
+                                    );
+                                })}
+                        </div>
+                    )}
+                </div>
+            )}
 
-      {/* The Starter Section */}
-      <div className="container">
-        <div className="row border-bottom border-2 border-secondary py-2">
-          <div className="col-auto fs-3">Where To Start</div>
-        </div>
+            <div className="container">
+                <div className="row border-bottom border-2 border-secondary py-2">
+                    <div className="col-auto fs-3">Validated Pathways</div>
+                </div>
+                {splashPageLoaderData.recentWork &&
+                    splashPageLoaderData.recentWork.map((pathway) => {
+                        return (
+                            <Link
+                                key={pathway.id}
+                                to={"pathway/" + pathway.id}
+                                className="row card text-decoration-none m-1 p-1 m-sm-2 p-sm-2"
+                            >
+                                <div className="fs-4 fw-bold">
+                                    {pathway.name}
+                                </div>
+                                <small className="text-muted">
+                                    Created By {pathway.author}
+                                </small>
+                            </Link>
+                        );
+                    })}
+            </div>
 
-        <div className="row py-2 mx-sm-2">
-          <div className="col p-0">
-            <StarterCard
-              title="Create A Pathway"
-              description="Create Your Own Pathway"
-              linkPath="build"
-            />
-          </div>
-          {/* TODO: Add groups/classrooms */}
-          {/* <div className="col p-0">
+            {/* The Starter Section */}
+            <div className="container">
+                <div className="row border-bottom border-2 border-secondary py-2">
+                    <div className="col-auto fs-3">Where To Start</div>
+                </div>
+
+                <div className="row py-2 mx-sm-2">
+                    <div className="col p-0">
+                        <StarterCard
+                            title="Create A Pathway"
+                            description="Create Your Own Pathway"
+                            linkPath="build"
+                        />
+                    </div>
+                    {/* TODO: Add groups/classrooms */}
+                    {/* <div className="col p-0">
                         <StarterCard
                             title="Find A Group"
                             description="Not Implimented"
                             linkPath="/explore/groups"/>
                     </div>*/}
-          {/* TODO: Add Community Creations */}
-          {/* <div className="col p-0">
+                    {/* TODO: Add Community Creations */}
+                    {/* <div className="col p-0">
                         <StarterCard 
                             title="Browse Community Creations" 
                             description="Not Implimented"
                             linkPath="/explore"/>
                     </div>  */}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 /**
@@ -119,19 +130,19 @@ const SplashPage = () => {
  * @returns
  */
 const StarterCard = (props) => {
-  return (
-    <Link
-      to={props.linkPath}
-      className="card h-100 text-start text-decoration-none"
-    >
-      <div className="row g-0">
-        <div className="card-body">
-          <div className="fs-3">{props.title}</div>
-          <div className="card-text">{props.description}</div>
-        </div>
-      </div>
-    </Link>
-  );
+    return (
+        <Link
+            to={props.linkPath}
+            className="card h-100 text-start text-decoration-none"
+        >
+            <div className="row g-0">
+                <div className="card-body">
+                    <div className="fs-3">{props.title}</div>
+                    <div className="card-text">{props.description}</div>
+                </div>
+            </div>
+        </Link>
+    );
 };
 
 export default SplashPage;
